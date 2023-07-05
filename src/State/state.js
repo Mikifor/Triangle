025c60a3d6477eaf40c71f87243ee2170959e2b1
Mikifor <mikifor.astro@gmail.com>
2023-07-05 22:45:05 +0500

    side = two angles complete

--- src/State/state.js
+++ src/State/state.js
@@ -2,32 +2,32 @@ let store = {
     _state: {
         Angles: [
             [
-                { path: ["Angles", "0", "0"], value: "49", title: "Угол A, градусы", type: "Number", key: "100", block: true },
-                { path: ["Angles", "0", "1"], value: "38", title: "Угол A, минуты", type: "Number", key: "110", block: true },
-                { path: ["Angles", "0", "2"], value: "56", title: "Угол A, секунды", type: "Number", key: "120", block: true }],
+                { path: ["Angles", "0", "0"], value: "137", title: "Угол A, градусы", type: "Number", key: "100", block: true },
+                { path: ["Angles", "0", "1"], value: "40", title: "Угол A, минуты", type: "Number", key: "110", block: true },
+                { path: ["Angles", "0", "2"], value: "12", title: "Угол A, секунды", type: "Number", key: "120", block: true }],
             [
-                { path: ["Angles", "1", "0"], value: "100", title: "Угол B, градусы", type: "Number", key: "101", block: true },
-                { path: ["Angles", "1", "1"], value: "50", title: "Угол B, минуты", type: "Number", key: "111", block: true },
-                { path: ["Angles", "1", "2"], value: "59", title: "Угол B, секунды", type: "Number", key: "121", block: true }],
+                { path: ["Angles", "1", "0"], value: "114", title: "Угол B, градусы", type: "Number", key: "101", block: true },
+                { path: ["Angles", "1", "1"], value: "45", title: "Угол B, минуты", type: "Number", key: "111", block: true },
+                { path: ["Angles", "1", "2"], value: "36", title: "Угол B, секунды", type: "Number", key: "121", block: true }],
             [
-                { path: ["Angles", "2", "0"], value: "71", title: "Угол C, градусы", type: "Number", key: "102", block: true },
-                { path: ["Angles", "2", "1"], value: "49", title: "Угол C, минуты", type: "Number", key: "112", block: true },
-                { path: ["Angles", "2", "2"], value: "54", title: "Угол C, секунды", type: "Number", key: "122", block: true }],
+                { path: ["Angles", "2", "0"], value: "106", title: "Угол C, градусы", type: "Number", key: "102", block: true },
+                { path: ["Angles", "2", "1"], value: "15", title: "Угол C, минуты", type: "Number", key: "112", block: true },
+                { path: ["Angles", "2", "2"], value: "36", title: "Угол C, секунды", type: "Number", key: "122", block: true }],
         ],
 
         Sides: [
             [
-                { path: ["Sides", "0", "0"], value: "50", title: "Сторона a, градусы", type: "Number", key: "200", block: true },
-                { path: ["Sides", "0", "1"], value: "52", title: "Сторона a, минуты", type: "Number", key: "210", block: true },
-                { path: ["Sides", "0", "2"], value: "48", title: "Сторона a, секунды", type: "Number", key: "220", block: true }],
+                { path: ["Sides", "0", "0"], value: "135", title: "Сторона a, градусы", type: "Number", key: "200", block: true },
+                { path: ["Sides", "0", "1"], value: "31", title: "Сторона a, минуты", type: "Number", key: "210", block: true },
+                { path: ["Sides", "0", "2"], value: "12", title: "Сторона a, секунды", type: "Number", key: "220", block: true }],
             [
-                { path: ["Sides", "1", "0"], value: "88", title: "Сторона b, градусы", type: "Number", key: "201", block: true },
-                { path: ["Sides", "1", "1"], value: "55", title: "Сторона b, минуты", type: "Number", key: "211", block: true },
-                { path: ["Sides", "1", "2"], value: "12", title: "Сторона b, секунды", type: "Number", key: "221", block: true }],
+                { path: ["Sides", "1", "0"], value: "109", title: "Сторона b, градусы", type: "Number", key: "201", block: true },
+                { path: ["Sides", "1", "1"], value: "7", title: "Сторона b, минуты", type: "Number", key: "211", block: true },
+                { path: ["Sides", "1", "2"], value: "48", title: "Сторона b, секунды", type: "Number", key: "221", block: true }],
             [
-                { path: ["Sides", "2", "0"], value: "75", title: "Сторона c, градусы", type: "Number", key: "202", block: true },
-                { path: ["Sides", "2", "1"], value: "18", title: "Сторона c, минуты", type: "Number", key: "212", block: true },
-                { path: ["Sides", "2", "2"], value: "0", title: "Сторона c, секунды", type: "Number", key: "222", block: true }],
+                { path: ["Sides", "2", "0"], value: "87", title: "Сторона c, градусы", type: "Number", key: "202", block: true },
+                { path: ["Sides", "2", "1"], value: "13", title: "Сторона c, минуты", type: "Number", key: "212", block: true },
+                { path: ["Sides", "2", "2"], value: "48", title: "Сторона c, секунды", type: "Number", key: "222", block: true }],
         ],
 
         Results: [
@@ -89,7 +89,7 @@ let store = {
     },
 
     putResults(props) {
-        debugger
+
         console.log(props)
         this._state.Results[0][0].word = props[3][0]
         this._state.Results[0][0].degree = props[0][0]
@@ -117,10 +117,8 @@ let store = {
         this.container();
     },
 
-    radians(itemPathOne, itemPathTwo, itemPathThree) {
-        return (Number(this._state[itemPathOne[0]][itemPathOne[1]][itemPathOne[2]].value) +
-            Number((this._state[itemPathTwo[0]][itemPathTwo[1]][itemPathTwo[2]].value / 60)) +
-            Number((this._state[itemPathThree[0]][itemPathThree[1]][itemPathThree[2]].value / 3600))) / 180 * Math.PI;
+    radians(item) {
+        return item / 180 * Math.PI;
     },
 
     degrees(rads) {
@@ -131,11 +129,18 @@ let store = {
         return ([degree, minute, second]);
     },
 
+    degreeInFraction(itemPathOne, itemPathTwo, itemPathThree) {
+        return (Number(this._state[itemPathOne[0]][itemPathOne[1]][itemPathOne[2]].value) +
+            Number((this._state[itemPathTwo[0]][itemPathTwo[1]][itemPathTwo[2]].value / 60)) +
+            Number((this._state[itemPathThree[0]][itemPathThree[1]][itemPathThree[2]].value / 3600)));
+
+    },
+
     threeSides() {
         debugger
-        let sideOneRad = this.radians(this._state.Sides[0][0].path, this._state.Sides[0][1].path, this._state.Sides[0][2].path);
-        let sideTwoRad = this.radians(this._state.Sides[1][0].path, this._state.Sides[1][1].path, this._state.Sides[1][2].path);
-        let sideThreeRad = this.radians(this._state.Sides[2][0].path, this._state.Sides[2][1].path, this._state.Sides[2][2].path);
+        let sideOneRad = this.radians(this.degreeInFraction(this._state.Sides[0][0].path, this._state.Sides[0][1].path, this._state.Sides[0][2].path));
+        let sideTwoRad = this.radians(this.degreeInFraction(this._state.Sides[1][0].path, this._state.Sides[1][1].path, this._state.Sides[1][2].path));
+        let sideThreeRad = this.radians(this.degreeInFraction(this._state.Sides[2][0].path, this._state.Sides[2][1].path, this._state.Sides[2][2].path));
 
         let cosAngleOne = (Math.cos(sideOneRad) - (Math.cos(sideTwoRad) * Math.cos(sideThreeRad))) / (Math.sin(sideTwoRad) * Math.sin(sideThreeRad));
         let cosAngleTwo = (Math.cos(sideTwoRad) - (Math.cos(sideThreeRad) * Math.cos(sideOneRad))) / (Math.sin(sideThreeRad) * Math.sin(sideOneRad));
@@ -148,9 +153,10 @@ let store = {
     },
 
     threeAngles() {
-        let angleOneRad = this.radians(this._state.Angles[0][0].path, this._state.Angles[0][1].path, this._state.Angles[0][2].path);
-        let angleTwoRad = this.radians(this._state.Angles[1][0].path, this._state.Angles[1][1].path, this._state.Angles[1][2].path);
-        let angleThreeRad = this.radians(this._state.Angles[2][0].path, this._state.Angles[2][1].path, this._state.Angles[2][2].path);
+        debugger
+        let angleOneRad = this.radians(this.degreeInFraction(this._state.Angles[0][0].path, this._state.Angles[0][1].path, this._state.Angles[0][2].path));
+        let angleTwoRad = this.radians(this.degreeInFraction(this._state.Angles[1][0].path, this._state.Angles[1][1].path, this._state.Angles[1][2].path));
+        let angleThreeRad = this.radians(this.degreeInFraction(this._state.Angles[2][0].path, this._state.Angles[2][1].path, this._state.Angles[2][2].path));
 
         let cosSideOne = (Math.cos(angleOneRad) + (Math.cos(angleTwoRad) * Math.cos(angleThreeRad))) / (Math.sin(angleTwoRad) * Math.sin(angleThreeRad));
         let cosSideTwo = (Math.cos(angleTwoRad) + (Math.cos(angleThreeRad) * Math.cos(angleOneRad))) / (Math.sin(angleThreeRad) * Math.sin(angleOneRad));
@@ -163,43 +169,59 @@ let store = {
     },
 
     sidePlusTwoAngles(props) {
-        let sideOneRad = 0
-        let angleTwoRad = 0
-        let angleThreeRad = 0
+        debugger
+        let sideOneFraction = 0
+        let angleTwoFraction = 0
+        let angleThreeFraction = 0
 
         switch (props) {
             case 0:
-                sideOneRad = this.radians(this._state.Sides[0][0].path, this._state.Sides[0][1].path, this._state.Sides[0][2].path)
-                angleTwoRad = this.radians(this._state.Angles[1][0].path, this._state.Angles[1][1].path, this._state.Angles[1][2].path)
-                angleThreeRad = this.radians(this._state.Angles[2][0].path, this._state.Angles[2][1].path, this._state.Angles[2][2].path)
+                sideOneFraction = this.degreeInFraction(this._state.Sides[0][0].path, this._state.Sides[0][1].path, this._state.Sides[0][2].path)
+                angleTwoFraction = this.degreeInFraction(this._state.Angles[1][0].path, this._state.Angles[1][1].path, this._state.Angles[1][2].path)
+                angleThreeFraction = this.degreeInFraction(this._state.Angles[2][0].path, this._state.Angles[2][1].path, this._state.Angles[2][2].path)
                 break
 
             case 1:
-                sideOneRad = this.radians(this._state.Sides[1][0].path, this._state.Sides[1][1].path, this._state.Sides[1][2].path)
-                angleTwoRad = this.radians(this._state.Angles[2][0].path, this._state.Angles[2][1].path, this._state.Angles[2][2].path)
-                angleThreeRad = this.radians(this._state.Angles[0][0].path, this._state.Angles[0][1].path, this._state.Angles[0][2].path)
+                sideOneFraction = this.degreeInFraction(this._state.Sides[1][0].path, this._state.Sides[1][1].path, this._state.Sides[1][2].path)
+                angleTwoFraction = this.degreeInFraction(this._state.Angles[2][0].path, this._state.Angles[2][1].path, this._state.Angles[2][2].path)
+                angleThreeFraction = this.degreeInFraction(this._state.Angles[0][0].path, this._state.Angles[0][1].path, this._state.Angles[0][2].path)
                 break
 
             case 2:
-                sideOneRad = this.radians(this._state.Sides[2][0].path, this._state.Sides[2][1].path, this._state.Sides[2][2].path)
-                angleTwoRad = this.radians(this._state.Angles[0][0].path, this._state.Angles[0][1].path, this._state.Angles[0][2].path)
-                angleThreeRad = this.radians(this._state.Angles[1][0].path, this._state.Angles[1][1].path, this._state.Angles[1][2].path)
+                sideOneFraction = this.degreeInFraction(this._state.Sides[2][0].path, this._state.Sides[2][1].path, this._state.Sides[2][2].path)
+                angleTwoFraction = this.degreeInFraction(this._state.Angles[0][0].path, this._state.Angles[0][1].path, this._state.Angles[0][2].path)
+                angleThreeFraction = this.degreeInFraction(this._state.Angles[1][0].path, this._state.Angles[1][1].path, this._state.Angles[1][2].path)
                 break
 
             default: break
         }
 
-        let cosAngleOne = (Math.cos(angleTwoRad) * Math.cos(angleThreeRad) * (-1)) + (Math.sin(angleTwoRad) * Math.sin(angleThreeRad) * Math.cos(sideOneRad))
 
-        let sinTeorema = Math.sin(sideOneRad) / Math.sqrt(1 - cosAngleOne * cosAngleOne)
+        let tanHalfSum = 0
+        let tanHalfDif = 0
+        if (angleTwoFraction > angleThreeFraction) {
+            tanHalfSum = Math.cos(this.radians((angleTwoFraction - angleThreeFraction) / 2)) / Math.cos(this.radians((angleTwoFraction + angleThreeFraction) / 2)) * Math.tan(this.radians(sideOneFraction / 2))
+            tanHalfDif = Math.sin(this.radians((angleTwoFraction - angleThreeFraction) / 2)) / Math.sin(this.radians((angleTwoFraction + angleThreeFraction) / 2)) * Math.tan(this.radians(sideOneFraction / 2))
+        } else {
+            tanHalfSum = Math.cos(this.radians((angleThreeFraction - angleTwoFraction) / 2)) / Math.cos(this.radians((angleTwoFraction + angleThreeFraction) / 2)) * Math.tan(this.radians(sideOneFraction / 2))
+            tanHalfDif = Math.sin(this.radians((angleThreeFraction - angleTwoFraction) / 2)) / Math.sin(this.radians((angleTwoFraction + angleThreeFraction) / 2)) * Math.tan(this.radians(sideOneFraction / 2))
+        }
+        if (tanHalfSum < 0) { tanHalfSum = (Math.PI + Math.atan(tanHalfSum)) }
+        else tanHalfSum = Math.atan(tanHalfSum)
 
-        let cosSideTwo = Math.sqrt(1 - ((Math.sin(angleTwoRad) * sinTeorema) * (Math.sin(angleTwoRad) * sinTeorema)));
-        let cosSideThree = Math.sqrt(1 - ((Math.sin(angleThreeRad) * sinTeorema) * (Math.sin(angleThreeRad) * sinTeorema)));
+
+        if (tanHalfDif < 0) { tanHalfDif = (Math.PI + Math.atan(tanHalfDif)) }
+        else tanHalfDif = Math.atan(tanHalfDif)
+
+        let sideTwoRad = (tanHalfSum + tanHalfDif)
+        let sideThreeRad = (tanHalfSum - tanHalfDif)
+
+        let cosAngleOne = (Math.cos(this.radians(angleTwoFraction)) * (-1)) * Math.cos(this.radians(angleThreeFraction)) + Math.sin(this.radians(angleTwoFraction)) * Math.sin(this.radians(angleThreeFraction)) * Math.cos(this.radians(sideOneFraction))
 
         const letter = (props) => {
             switch (props) {
                 case 0: return ["A", "B", "C"]
-                case 1: return ["B", "C", "A"]
+                case 1: return ["B", "A", "C"]
                 case 2: return ["C", "A", "B"]
                 default: break
             }
@@ -207,11 +229,11 @@ let store = {
 
         const word = ["Угол", "Сторона", "Сторона"]
 
-        this.putResults([this.degrees(Math.acos(cosAngleOne)), this.degrees(Math.acos(cosSideTwo)), this.degrees(Math.acos(cosSideThree)), word, letter(props)]);
+        this.putResults([this.degrees(Math.acos(cosAngleOne)), this.degrees(sideTwoRad), this.degrees(sideThreeRad), word, letter(props)]);
     },
 
     anglePlusTwoSides(props) {
-        debugger
+
         let angleOneRad = 0
         let tanAngleTwo = 0
         let tanAngleThree = 0
@@ -221,37 +243,34 @@ let store = {
         switch (props) {
 
             case 0:
-                angleOneRad = this.radians(this._state.Angles[0][0].path, this._state.Angles[0][1].path, this._state.Angles[0][2].path)
-                sideTwoRad = this.radians(this._state.Sides[1][0].path, this._state.Sides[1][1].path, this._state.Sides[1][2].path)
-                sideThreeRad = this.radians(this._state.Sides[2][0].path, this._state.Sides[2][1].path, this._state.Sides[2][2].path)
+                angleOneRad = this.radians(this.degreeInFraction(this._state.Angles[0][0].path, this._state.Angles[0][1].path, this._state.Angles[0][2].path))
+                sideTwoRad = this.radians(this.degreeInFraction(this._state.Sides[1][0].path, this._state.Sides[1][1].path, this._state.Sides[1][2].path))
+                sideThreeRad = this.radians(this.degreeInFraction(this._state.Sides[2][0].path, this._state.Sides[2][1].path, this._state.Sides[2][2].path))
                 break
 
             case 1:
-                angleOneRad = this.radians(this._state.Angles[1][0].path, this._state.Angles[1][1].path, this._state.Angles[1][2].path)
-                sideTwoRad = this.radians(this._state.Sides[0][0].path, this._state.Sides[0][1].path, this._state.Sides[0][2].path)
-                sideThreeRad = this.radians(this._state.Sides[2][0].path, this._state.Sides[2][1].path, this._state.Sides[2][2].path)
+                angleOneRad = this.radians(this.degreeInFraction(this._state.Angles[1][0].path, this._state.Angles[1][1].path, this._state.Angles[1][2].path))
+                sideTwoRad = this.radians(this.degreeInFraction(this._state.Sides[0][0].path, this._state.Sides[0][1].path, this._state.Sides[0][2].path))
+                sideThreeRad = this.radians(this.degreeInFraction(this._state.Sides[2][0].path, this._state.Sides[2][1].path, this._state.Sides[2][2].path))
                 break
 
             case 2:
-                angleOneRad = this.radians(this._state.Angles[2][0].path, this._state.Angles[2][1].path, this._state.Angles[2][2].path)
-                sideTwoRad = this.radians(this._state.Sides[1][0].path, this._state.Sides[1][1].path, this._state.Sides[1][2].path)
-                sideThreeRad = this.radians(this._state.Sides[0][0].path, this._state.Sides[0][1].path, this._state.Sides[0][2].path)
+                angleOneRad = this.radians(this.degreeInFraction(this._state.Angles[2][0].path, this._state.Angles[2][1].path, this._state.Angles[2][2].path))
+                sideTwoRad = this.radians(this.degreeInFraction(this._state.Sides[1][0].path, this._state.Sides[1][1].path, this._state.Sides[1][2].path))
+                sideThreeRad = this.radians(this.degreeInFraction(this._state.Sides[0][0].path, this._state.Sides[0][1].path, this._state.Sides[0][2].path))
                 break
 
             default: break
         }
 
         tanAngleThree = Math.sin(angleOneRad) / ((Math.sin(sideTwoRad) / Math.tan(sideThreeRad)) - Math.cos(angleOneRad) * Math.cos(sideTwoRad))
-        if (tanAngleThree < 0) { tanAngleThree = (Math.atan(tanAngleThree) + Math.PI) }
+        if (tanAngleThree < 0) { tanAngleThree = (Math.PI + Math.atan(tanAngleThree)) }
+        else tanAngleThree = Math.atan(tanAngleThree)
 
         cosSideOne = (Math.cos(sideTwoRad) * Math.cos(sideThreeRad)) + (Math.sin(sideTwoRad) * Math.sin(sideThreeRad) * Math.cos(angleOneRad))
-        let sideOneRad = Math.acos(cosSideOne)
 
-
-        let angleThreeRad = Math.atan(tanAngleThree)
-
-        tanAngleTwo = Math.sin(angleThreeRad) / ((Math.sin(sideOneRad) / Math.tan(sideTwoRad)) - Math.cos(angleThreeRad) * Math.cos(sideOneRad))
-        if ((tanAngleTwo) < 0) { tanAngleTwo = (Math.atan(tanAngleTwo) + Math.PI) }
+        tanAngleTwo = Math.sin(angleOneRad) / ((Math.sin(sideThreeRad) / Math.tan(sideTwoRad)) - Math.cos(angleOneRad) * Math.cos(sideThreeRad))
+        if (tanAngleTwo < 0) { tanAngleTwo = (Math.PI + Math.atan(tanAngleTwo)) }
         else tanAngleTwo = Math.atan(tanAngleTwo)
 
         const letter = (props) => {
@@ -265,7 +284,7 @@ let store = {
 
         const word = ["Сторона", "Угол", "Угол"]
 
-        this.putResults([this.degrees(Math.acos(cosSideOne)), this.degrees(tanAngleTwo), this.degrees(Math.atan(tanAngleThree)), word, letter(props)])
+        this.putResults([this.degrees(Math.acos(cosSideOne)), this.degrees(tanAngleTwo), this.degrees(tanAngleThree), word, letter(props)])
     },
 
     switchBlock(props) {
